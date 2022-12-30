732760f168619b64afbcb38505c6e0e48c04fb2f
miguelta281 <miguelta281@gmail.com>
2022-12-30 17:28:56 -0500

    Cambiando la forma de importar el archivo service

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ingles',
-    version='1.0',
+    version='1.1',
     description='Un modulo para repasar ingles',
     author='Miguel Tarazona',
     author_email='miguelta281@gmail.com',
