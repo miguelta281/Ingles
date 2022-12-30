62338a4d7129d635cb1fab09dcaffb90c53fcec9
miguelta281 <miguelta281@gmail.com>
2022-12-30 17:13:07 -0500

    Versión uno del módulo de ejercicios de ingles

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ingles',
+    version='1.0',
+    description='Un modulo para repasar ingles',
+    author='Miguel Tarazona',
+    author_email='miguelta281@gmail.com',
+    packages=['ingles'],
+    install_requires=['json'],
+)
