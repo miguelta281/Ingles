eda7a2906e5539657a7f791e4bc066a2a34f3350
miguelta281 <miguelta281@gmail.com>
2022-12-30 17:25:36 -0500

    Agregando al ópción de la ruta del archivo

--- ingles/languageExerciseGenerator.py
+++ ingles/languageExerciseGenerator.py
@@ -1,4 +1,4 @@
-from service import add_words, review_vocabulary
+from ingles.service import  add_words, review_vocabulary
 
 def study_vocabulary (file_path):
     print(f'Opciones: 1-> Agregar una nueva palabra, 2 -> repasar' )
